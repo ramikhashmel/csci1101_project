eabd3eabf5835e147b440e2f8117d87cd95eaa49
Alex Yorke <alhexyorke@users.noreply.github.com>
2017-03-23 16:50:13 -0300

    Add docs

--- ATM.java
+++ ATM.java
@@ -5,25 +5,31 @@ public class ATM {
 
 	// entry point
 	public static void main(String[] args) throws Exception {
+		
+		// create the mvc
 		View view = new View();
 		Model model = new Model();
 		Controller controller = new Controller();
 
+		// register them with each other
 		controller.addView(view);
 		controller.addModel(model);
 		view.addController(controller);
 
+		// create new account, and ask user for a card number
 		Account acc = new Account();
 		String cardNumber = UserInput.getString("Card Number: ", Restrictions.getCCNumberRestriction());
 
 		Card card = new Card();
 		card.setNumber(cardNumber);
 
+		// get pin, and try to validate the card
 		String pin = UserInput.getString("PIN: ", Restrictions.getPinRestriction());
 
 		InputRestrictionResult result = null;
 		ViewState state = new ViewState();
 
+		// if it's valid, continue otherwise abort
 		if (model.isValidCard(card)) {
 			view.update(ViewState.CARD_VALID, result);
 			view.update(ViewState.CARD_WITHDRAW_OR_DEPOSIT);
