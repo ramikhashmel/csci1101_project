57e47022db88c88503ef18490669ab6528b80601
Alex Yorke <alhexyorke@users.noreply.github.com>
2017-03-25 20:58:16 -0300

    Remove unneeded update() overloads in view

--- View.java
+++ View.java
@@ -21,20 +21,9 @@ public class View {
 		this.card = card;
 	}
 
-	public void update() {
-		// TODO Auto-generated method stub
-		// refreshes the view with new data that it now has access to
-	}
-
 	public void update(ViewState state) {
 		// this would fire when the input does not have to be restricted
 		System.out.println("update() called with new view state " + state.name());
-	}
-
-	public void update(ViewState state, Card card) {
-		// update the screen, but have the card information handy to verify with
-		// the controller
-		// if the card is valid and has enough money
 	}
 
 	public void update(ViewState state, InputRestrictionResult result) {
