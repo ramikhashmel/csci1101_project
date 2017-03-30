84133944ae4e60aff4dab5a04120c3464f2ce069
ramikhashmel <Ramster@Ramis-MacBook-Pro.local>
2017-03-30 02:28:48 -0300

    Number Pad for pin added
    Will add sound effects and clicking feature tomorrow

--- View.java
+++ View.java
@@ -14,12 +14,15 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.media.AudioClip;
 
 //http://www.austintek.com/mvc/#austintek_mvc.view_2
 
 public class View extends Application {
 	static public Controller controller;
 	public static Stage primaryStage;
+   AudioClip audio;
+   Button button;
 
 	public static void initialize(String[] args) {
 		launch(args);
@@ -68,7 +71,17 @@ public class View extends Application {
 	// http://docs.oracle.com/javafx/2/get_started/form.htm#BABDDGEE
 	public void start(Stage primaryStage) {
 
-		this.primaryStage = primaryStage;
+		//String clipURL= "http://www.music.helsinki.fi//tmt/opetus/uusmedia/esim/a2002011001-e02.wav";
+      //audio = new AudioClip(clipURL);
+      String[] keys =
+        {
+            "1", "2", "3",
+            "4", "5", "6",
+            "7", "8", "9",
+            "Enter", "Cancel", "Back"
+        };
+        
+      this.primaryStage = primaryStage;
 		// create the initial screen the user will see
 		primaryStage.setTitle("Welcome to our ATM Demo");
 
@@ -79,22 +92,30 @@ public class View extends Application {
 		grid.setPadding(new Insets(25, 25, 25, 25));
 
 		Scene scene = new Scene(grid, 600, 400);
+      
+      for (int i = 0; i < 12; i++)
+        {
+            button = new Button(keys[i]);
+            button.getStyleClass().add("num-button");
+            grid.add(button, i % 3, (int) Math.ceil(i / 3));
+     
+        }
 
 		Text scenetitle = new Text("Enter Account Information");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 2, 1);
+		grid.add(scenetitle, 4, 0, 1, 1);
 
 		Label cardNumber = new Label("Card Number:");
-		grid.add(cardNumber, 0, 1);
+		grid.add(cardNumber, 5, 0);
 
 		final TextField cardNumberField = new TextField();
-		grid.add(cardNumberField, 1, 1);
+		grid.add(cardNumberField, 5, 1);
 
 		Label pin = new Label("PIN:");
-		grid.add(pin, 0, 2);
+		grid.add(pin, 5, 2);
 
 		final PasswordField pinField = new PasswordField();
-		grid.add(pinField, 1, 2);
+		grid.add(pinField, 5, 3);
 
 		Button signIn = new Button("Go");
 		HBox hbBtn = new HBox(10);
@@ -106,10 +127,16 @@ public class View extends Application {
 		grid.add(actiontarget, 1, 6);
 
 		signIn.setOnAction(new MainMenu(cardNumberField, pinField, actiontarget));
-
-		primaryStage.setScene(scene);
+      //button.setOnAction(this::processButtonPress);
+		
+      primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+   /*
+   public void processButtonPress(ActionEvent event){
+
+   }
+   */
 
 	public void update(ViewState state) {
 		// this would fire when the input does not have to be restricted
