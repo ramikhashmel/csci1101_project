a415f522772a50b2dbcd2ae4e5d051e4ebe8e742
Alex Yorke <alhexyorke@users.noreply.github.com>
2017-03-30 17:38:41 -0300

    Merge Restrictions into UserInput

--- UserInput.java
+++ UserInput.java
@@ -221,4 +221,34 @@ class InputRestriction extends UserInput {
 
 }
 
+/*
+ * This class provides common restrictions for keyboard input. For example,
+ * the credit card restriction only allows 16 numbers. The pin restriction
+ * only allows four numbers.
+ */
+abstract class Restrictions extends InputRestriction {
+	/*
+	 * The input restriction for asking the user for their card number
+	 * (has to be exactly 16 digits)
+	 */
+	static public InputRestriction CCNumber() {
+		InputRestriction cardNumberRestriction = new InputRestriction();
+		cardNumberRestriction.setShouldBeNumeric(true);
+		cardNumberRestriction.setMaxLength(16);
+		cardNumberRestriction.setMinLength(16);
+		return cardNumberRestriction;
+	}
+
+	/*
+	 * Input restriction for asking the user for their pin
+	 * (has to be exactly four digits)
+	 */
+	static public InputRestriction pin() {
+		InputRestriction pinRestriction = new InputRestriction();
+		pinRestriction.setShouldBeNumeric(true);
+		pinRestriction.setMaxLength(4);
+		pinRestriction.setMinLength(4);
+		return pinRestriction;
+	}
+}
 
