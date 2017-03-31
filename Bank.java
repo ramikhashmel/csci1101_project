dbe814a11f656af1a65d47167067b79dca7874f3
Alex Yorke <alhexyorke@users.noreply.github.com>
2017-03-30 22:28:48 -0300

    Remove unused methods in Bank

--- Bank.java
+++ Bank.java
@@ -7,38 +7,6 @@ public class Bank {
   int branchCode;
 
   /**
-   * Adds a person to the current account
-   */
-  public void addPerson() {
-
-  }
-
-  /**
-   * Adds a person to the account
-   * 
-   * @param acc The account
-   */
-  public void addPerson(Account acc) {
-
-  }
-
-  /**
-   * Deposits money to the current account
-   */
-  public void deposit() {
-
-  }
-
-  /**
-   * Deposits money to the specified account
-   * 
-   * @param account The account to deposit money into
-   */
-  public void deposit(Account account) {
-
-  }
-
-  /**
    * Transfers the money from one account to another one
    */
   /**
@@ -46,13 +14,6 @@ public class Bank {
    * @param dest The destination account
    */
   public void transfer(Account source, Account dest) {
-
-  }
-
-  /**
-   * Withdraws money from the current account
-   */
-  public void withdraw() {
 
   }
 }
